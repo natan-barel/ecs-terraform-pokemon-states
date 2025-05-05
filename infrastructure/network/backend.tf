e93ebfede3e26a96cd9ae5e59c0860e6dc599eaa
natan-barel <natan.barel@outlook.co.il>
2025-05-05 21:30:24 +0300

    Revert backend.tf to use static bucket name

--- infrastructure/network/backend.tf
+++ infrastructure/network/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-states-unique-${data.aws_caller_identity.current.account_id}"
+    bucket         = "my-terraform-states-unique"
     key            = "network/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
