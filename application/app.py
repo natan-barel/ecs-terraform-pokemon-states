e22c23fd80309cdc6f6acaee444c46eeed1129b9
natan-barel <natan.barel@outlook.co.il>
2025-05-06 02:22:34 +0300

    feat: add server IP and AZ display to web interface

--- application/app.py
+++ application/app.py
@@ -4,8 +4,27 @@ from flask import Flask, render_template, jsonify
 import json
 import random
 import os
+import socket
+import requests
 
 app = Flask(__name__)
+
+def get_server_info():
+    # Get server IP
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+    
+    # Get Availability Zone from AWS metadata
+    try:
+        az_response = requests.get('http://169.254.169.254/latest/meta-data/placement/availability-zone', timeout=1)
+        az = az_response.text
+    except:
+        az = "Not available (not running in AWS)"
+    
+    return {
+        'ip': ip,
+        'az': az
+    }
 
 def get_random_pokemon():
     json_path = os.path.join(os.path.dirname(__file__), 'AllPokemons.json')
@@ -16,7 +35,8 @@ def get_random_pokemon():
 @app.route('/')
 def index():
     random_pokemon = get_random_pokemon()
-    return render_template('index.html', pokemon=random_pokemon)
+    server_info = get_server_info()
+    return render_template('index.html', pokemon=random_pokemon, server_info=server_info)
 
 @app.route("/health")
 def health_check():
