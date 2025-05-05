d1417548a01448713f2c2ea6b8998b9de3359af9
natan-barel <natan.barel@outlook.co.il>
2025-05-05 21:25:51 +0300

    Update network backend.tf to use account-specific bucket name

--- infrastructure/network/backend.tf
+++ infrastructure/network/backend.tf
@@ -1,7 +1,6 @@
-
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-states-unique"
+    bucket         = "my-terraform-states-unique-${data.aws_caller_identity.current.account_id}"
     key            = "network/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
